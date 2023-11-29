5917b11c9201849f06124691dfcb52b915ae1992
Demian Binimelis <dbinimelis2022@alu.uct.cl>
2023-11-28 23:53:49 -0300

    Actualizacion con tiburon

--- Ambiente.py
+++ Ambiente.py
@@ -1,13 +1,17 @@
+from random import randint
 class Ambiente:
     def __init__(self, Temperatura, Salinidad):
         self.Temperatura = Temperatura
         self.Salinidad = Salinidad
         
     def Marea_Roja(self):
-        if((self.Salinidad > 20) or (self.Temperatura > 30)):
+
+        probabilidadMaRed = randint(1,100)
+
+        if((self.Salinidad > 20) or (self.Temperatura > 30)) and probabilidadMaRed <=50:
             print("Hay marea roja")
         else:
-            print("No hay peligro de marea roja")
-            
+            print("No hay marea roja")
+        
     def Marea_Negra(self):
         print("Un barco a derramado petroleo")
--- Main.py
+++ Main.py
@@ -1,28 +1,56 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
+from Ambiente import Ambiente 
+from random import randint
 
 pygame.init()
 
 Ancho, Alto = 528, 396
-Anch_Sprite, Alt_Sprite = 100, 100
+Anch_Sprite, Alt_Sprite = 30, 30
 
 Pantalla = pygame.display.set_mode((Ancho, Alto))
 pygame.display.set_caption("Progra_Ecosistema")
 
-imagen_agua = pygame.image.load("Sprites/Agua.png").convert_alpha()
+imagen_Tiburon = pygame.image.load("Sprites/TB.png")
+imagen_agua = pygame.image.load("Sprites/Agua.png")
+
+ambiente=Ambiente(Temperatura=25,Salinidad=25)
 
 
-Escen_Panta=[
-    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+
+Escen_Panta = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
+
+EvalMar = False
+
+reloj = pygame.time.Clock() 
+
+movimientos = {0: (0, -1),  # Arriba
+               1: (0, 1),   # Abajo
+               2: (-1, 0),  # Izquierda
+               3: (1, 0)}   # Derecha
+
+
+direccion = randint(0, 3)
+
+ambiente.posicion_tiburon = [0, 0]
+Pantalla.blit(imagen_Tiburon, (ambiente.posicion_tiburon[0] * Anch_Sprite, ambiente.posicion_tiburon[1] * Alt_Sprite))
+
 
 while True:
     for event in pygame.event.get():
@@ -30,7 +58,17 @@ while True:
             pygame.quit()
             sys.exit()
 
-    Pantalla.fill((255, 255, 255))  # Llenar la pantalla con un color de fondo
+    if not EvalMar:
+        ambiente.Marea_Roja()
+        EvalMar = True
+
+    # Cambio la direccion
+    direccion = randint(0, 3)
+
+    # Aplicacion del tiburon
+    dx, dy = movimientos[direccion]
+    ambiente.posicion_tiburon[0] = max(0, min(ambiente.posicion_tiburon[0] + dx, len(Escen_Panta[0]) - 1))
+    ambiente.posicion_tiburon[1] = max(0, min(ambiente.posicion_tiburon[1] + dy, len(Escen_Panta) - 1))
 
     for fila in range(len(Escen_Panta)):
         for columna in range(len(Escen_Panta[fila])):
@@ -40,5 +78,7 @@ while True:
                 except pygame.error as e:
                     print(f"Error al cargar la imagen: {e}")
 
+    Pantalla.blit(imagen_Tiburon, (ambiente.posicion_tiburon[0] * Anch_Sprite, ambiente.posicion_tiburon[1] * Alt_Sprite))
+
     pygame.display.flip()
-  
+    reloj.tick(5)
